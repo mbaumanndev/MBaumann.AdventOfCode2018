205a0de7495e633ba44dd4c296f2c91fe87dde49
Maxime BAUMANN <maxime.baumann.pro@gmail.com>
2018-12-18 15:43:32 +0100

    Day 2 part 1

---
+++ test/MBaumann.AdventOfCode.Day1.Test/MBaumann.AdventOfCode.Day2.Test/Part1UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MBaumann.AdventOfCode.Day2.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
